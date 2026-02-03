ef4dc62e24cdbd3ed9936eee99fe988ebb2d7aa8
gayatree811 <gayatrigayatriparida133@gmail.com>
2026-02-03 20:14:46 +0530

    Added mouseover events and updated DOM files

--- Event/mouseover.js
+++ Event/mouseover.js
@@ -1,12 +1,12 @@
 let abc = document.querySelector("#abc");
 
-/* abc.addEventListener("mouseover",function(){
+ abc.addEventListener("mouseover",function(){
     abc.style.backgroundColor = "yellow";
 });
 
 abc.addEventListener("mouseout",function(){
     abc.style.backgroundColor = "blue";
-}); */
+}); 
 
 window.addEventListener("mousemove", function(data){
     abc.style.top= data.clientY + "PX";
